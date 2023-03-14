8b0a941ddeec1cb40a89405b9571cca42f9abe36
Taitaitatata <70869837+Taitaitatata@users.noreply.github.com>
2023-03-14 19:42:09 +0900

    Update: 自殺後座標を送るように

--- nekohub-spigot/src/main/java/com/nekozouneko/nekohub/spigot/task/SuicideTask.java
+++ nekohub-spigot/src/main/java/com/nekozouneko/nekohub/spigot/task/SuicideTask.java
@@ -1,29 +1,44 @@
 package com.nekozouneko.nekohub.spigot.task;
 
+import com.nekozouneko.nekohub.spigot.SpigotUtil;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public final class SuicideTask extends BukkitRunnable {
 
     private final Player p;
+    private final Location loc;
     private int second;
-    private boolean first;
+    private boolean first = true;
 
     public SuicideTask(Player player, int second) {
         this.p = player;
         this.second = second;
+        this.loc = p.getLocation().clone();
     }
 
     @Override
     public void run() {
         if (first) {
             first = false;
+            p.sendMessage("§c§n自殺を" + second + "秒後実行します。キャンセルするには動いてください。");
         }
         else second--;
 
-        if (second < 0) {
-            p.setHealth(0);
+        if (!loc.equals(p.getLocation())) {
+            p.sendMessage("自殺をキャンセルしました。");
+            cancel();
+            return;
+        }
 
+        if (second < 1) {
+            p.sendMessage("自殺を実行しました: " + SpigotUtil.locationFormat("%.03f / %.03f / %.03f (%s)", p.getLocation()));
+            p.setHealth(0);
+            cancel();
+        }
+        else {
+            p.sendMessage(second + "秒後自殺を実行します。");
         }
     }
 }
